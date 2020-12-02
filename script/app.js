982dba5b8347b91d9dccedd4f6819a87837dd36f
kristijan10 <44801324+kristijan10@users.noreply.github.com>
2020-12-02 15:02:00 +0100

    added animations

--- script/app.js
+++ script/app.js
@@ -10,3 +10,37 @@ function toggleMenu(e) {
 }
 
 document.body.addEventListener('click', toggleMenu);
+
+
+// Functija za animaciju
+// Kada je visina browsera na elementu, doda mu se klasa, a i ukloni
+(function () {
+    var elements;
+    var windowHeight;
+
+    function init() {
+        elements = document.querySelectorAll('.hidden');
+        windowHeight = window.innerHeight;
+    }
+
+    function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight <= 0 && element.classList.contains('animate__animated')) {
+                element.classList.add('animate__slideInLeft');
+                element.classList.remove('hidden');
+            } else if (positionFromTop - windowHeight <= 0) {
+                element.classList.add('fade-in');
+                element.classList.remove('hidden');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+})();
