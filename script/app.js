d84aff90ce0640a2bfd257275e2d601202d8302b
kristijan10 <44801324+kristijan10@users.noreply.github.com>
2020-12-03 20:32:07 +0100

    added google map api

--- script/app.js
+++ script/app.js
@@ -65,7 +65,29 @@ nextPicture.addEventListener('click', () => {
 
 
 // KONTAKT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-let map = document.querySelector('iframe');
-map.addEventListener('mouseover', () => {
-    document.body.style.backgroundColor = 'black';
-})
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 44.758663360782144,
+            lng: 20.26395009829545
+        },
+        zoom: 15,
+        mapTypeId: 'hybrid'
+    });
+
+    marker = new google.maps.Marker({
+        position: {
+            lat: 44.758663360782144,
+            lng: 20.26395009829545
+        },
+        map: map
+    });
+
+    let infoWindow = new google.maps.InfoWindow({
+        content: '<h2>Pansion "Simba"</h2>'
+    });
+
+    marker.addListener('mouseover', function () {
+        infoWindow.open(map, marker);
+    });
+}
