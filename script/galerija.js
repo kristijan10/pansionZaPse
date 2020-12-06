4cc76a7e73d4087971e8ef7819c54911f93d2c10
kristijan10 <44801324+kristijan10@users.noreply.github.com>
2020-12-06 12:48:18 +0100

    'galerija' done

---
+++ script/galerija.js
@@ -0,0 +1,68 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function plusSlide(n) {
+    showSlide(slideIndex += n);
+};
+
+function showSlide(n) {
+    const pictures = document.getElementsByClassName('picture');
+    const dots = document.getElementsByClassName('dot');
+
+    if (n > pictures.length) slideIndex = 1;
+    if (n < 1) slideIndex = pictures.length;
+    for (let i = 0; i < pictures.length; i++)
+        pictures[i].style.display = 'none';
+    for (let i = 0; i < dots.length; i++)
+        dots[i].className = dots[i].className.replace(' active', '');
+    pictures[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+}
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches || // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            /* left swipe */
+            plusSlide(-1);
+        } else {
+            /* right swipe */
+            plusSlide(1);
+        }
+    } else {
+        if (yDiff > 0) {
+            /* up swipe */
+        } else {
+            /* down swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
